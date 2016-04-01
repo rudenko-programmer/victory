45ddae49d84e36e62b5d0b81d9d37564a9ebcd5c
Maksim Rudenko <rudenko.programmer@gmail.com>
2016-04-01 17:16:19 +0300

    Чоздание основных директорий

---
+++ victory/victory.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Victory We simplify your life
+Description: Проэкт предназначен для автоматизации создания произвольных типов поста, таксономий, дополнительных блоков
+Version: 1.0
+Author: Rudenko Maksim
+Author URI: https://github.com/rudenko-programmer
+Plugin URI: https://github.com/rudenko-programmer/victory
+*/

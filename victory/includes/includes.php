de92719f19702c9667276217a3191880630d1d5f
Maksim Rudenko <rudenko.programmer@gmail.com>
2016-04-03 02:18:59 +0300

    Изменение, добавлен тип записи для Типов записи, добавлены дополнительные поля

---
+++ victory/includes/includes.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Подключение файлов директории Includes
+ */
+require VICTORY_DIR.'/includes/admin/victory_post_type.php';
+
+require VICTORY_DIR.'/includes/admin/victory_meta_box.php';
--- victory/victory.php
+++ victory/victory.php
@@ -1,9 +1,32 @@
 <?php
 /*
-Plugin Name: Victory We simplify your life
+Plugin Name: Victory - my WordPress solution
 Description: Проэкт предназначен для автоматизации создания произвольных типов поста, таксономий, дополнительных блоков
 Version: 1.0
 Author: Rudenko Maksim
 Author URI: https://github.com/rudenko-programmer
 Plugin URI: https://github.com/rudenko-programmer/victory
 */
+?>
+<?php
+/**
+ * Инициализация основных глобальных констант
+ */
+define('VICTORY_DIR', plugin_dir_path(__FILE__));
+define('VICTORY_URL', plugin_dir_url(__FILE__));
+define('WP_POST_REVISIONS', false);
+/**
+ * Инициализация основных стилей и скриптов
+ */
+function victory_load_style(){ wp_enqueue_style( 'victory_style', VICTORY_URL.'/assets/css/style.css' );}
+function victory_load_script() {
+    if ( ! did_action( 'wp_enqueue_media' ) ){ wp_enqueue_media(); }
+    wp_enqueue_script( 'victory_script', VICTORY_URL.'/assets/js/script.js', array('jquery'), null, false );
+}
+add_action( 'admin_enqueue_scripts', 'victory_load_style' );
+add_action( 'admin_enqueue_scripts', 'victory_load_script' );
+
+/**
+ * Инициализация файлов директории INCLUDES
+ */
+require VICTORY_DIR.'/includes/includes.php';
